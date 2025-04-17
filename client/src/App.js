f185f75ffbcf04aa13867cf2d57e69d906711b41
Ayush Gour <aayushgour77@gmail.com>
2025-04-17 12:50:44 +0530

    Remove boilerplate and add react route

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Navbar from "./components/layout/Navbar";
+import Landing from "./components/layout/Landing";
+import Login from "./components/auth/Login";
+import Register from "./components/auth/Register";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => (
+  <Router>
+    <>
+      <Navbar />
+      <Routes>
+        <Route path='/' element={<Landing />} />
+        <Route path='/register' element={
+          <section className="container">
+            <Register />
+          </section>
+        } />
+        <Route path='/login' element={
+          <section className="container">
+            <Login />
+          </section>
+        } />
+      </Routes>
+    </>
+  </Router>
+);
 
 export default App;
